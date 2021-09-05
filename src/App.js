61f851bca406af0b35b3fa5b9c3fbd3eedc03b60
Qutadah95 <qutadaalmomani@yahoo.com>
2021-09-05 22:07:36 +0300

    try & catche

--- src/App.js
+++ src/App.js
@@ -9,64 +9,77 @@ export class App extends Component {
     this.state = {
       Name: '',
       Data: {},
-      map_url:'',
+      map_url: '',
+      error: false,
+
     }
   };
   NameChange = (e) => { this.setState({ Name: e.target.value }) }
   handelSubmit = async (e) => {
 
     e.preventDefault();
-    console.log(this.state.Name);
+    try {
+      console.log(this.state.Name);
 
 
 
-    const url = `https://eu1.locationiq.com/v1/search.php?key=${process.env.REACT_APP_API_KEY}&q=${this.state.Name}&format=json`;
+      const url = `https://eu1.locationiq.com/v1/search.php?key=${process.env.REACT_APP_API_KEY}&q=${this.state.Name}&format=json`;
 
-   
-    
-    const response = await axios.get(url);
-    
-    console.log(response.data[0]);
-    this.setState({
-      Data: response.data[0],
-      
-    });
-    const map_url=`https://maps.locationiq.com/v3/staticmap?key=${process.env.REACT_APP_API_KEY}&center=${this.state.Data.lat},${this.state.Data.lon}&format=png`;
-    console.log(map_url);
-    this.setState({
-      map_url:map_url,
-      
-    });
+
+
+      const response = await axios.get(url);
+
+      console.log(response.data[0]);
+      this.setState({
+        Data: response.data[0],
+
+      });
+      const map_url = `https://maps.locationiq.com/v3/staticmap?key=${process.env.REACT_APP_API_KEY}&center=${this.state.Data.lat},${this.state.Data.lon}&format=png`;
+      console.log(map_url);
+      this.setState({
+        map_url: map_url,
+
+      });
+    } catch (error) {
+      this.setState({
+        error: true,
+      });
+    }
   }
-  
-  render(){
-    
-  return (
-    <div >
-<Form onSubmit={this.handelSubmit}>
-  <Form.Group onChange={this.NameChange} className="mb-3" controlId="formBasicEmail">
-    <Form.Label>location Name</Form.Label>
-    <Form.Control type="text" placeholder="Enter the location Name" />
-  
-  </Form.Group>
 
- 
- 
-  <Button variant="primary" type="submit">
-    Submit
-  </Button>
-</Form>
-<div>
+
+  render() {
+console.log(this.state.error);
+    return (
+      <div >
+        <Form onSubmit={this.handelSubmit}>
+          <Form.Group onChange={this.NameChange} className="mb-3" controlId="formBasicEmail">
+            <Form.Label>location Name</Form.Label>
+            <Form.Control type="text" placeholder="Enter the location Name" />
+
+          </Form.Group>
+
+
+
+          <Button variant="primary" type="submit">
+            Submit
+          </Button>
+        </Form>
+        <div>
+          
           <h2>location informations</h2>
           <p>location: {this.state.Data.display_name}</p>
           <p>latitude: {this.state.Data.lat}</p>
           <p>longitude: {this.state.Data.lon}</p>
-          <img src={this.state.map_url} alt=""/>
-          
+          {this.state.error &&<p>error getting the data  </p>}
+          <img src={this.state.map_url} alt="" />
+
+
         </div>
-        
-    </div>
-  );
-}}
+
+      </div>
+    );
+  }
+}
 
 export default App;
